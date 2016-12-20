3d3ba9ba910aac8b17874517d260202b4e26d633
balpandeyogita <yogitabalpande@gmail.com>
2016-12-20 10:48:10 +0530

    msg

---
+++ src/person/Ex.java
@@ -0,0 +1,29 @@
+package person;
+
+public class Ex {
+
+//	public static void main(String[] args) {
+//		int x=10;
+//		int y= 20;
+//		x=x+y;
+//		y=x-y;
+//		x=x-y;
+//		
+//		System.out.println("x:"+x);
+//		System.out.println("y:"+y);
+//		
+//	}
+	
+	
+	static int num=20;
+	   static String mystr="pqr";
+	   static{
+	      num = 97;
+	      mystr = "Static keyword in Java";
+	   }
+	   public static void main(String args[])
+	   {
+	      System.out.println("Value of num="+num);
+	      System.out.println("Value of mystr="+mystr);
+	   }
+}
